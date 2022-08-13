851e922afef270885991d749db6fd4241db38ff8
unknown <mirtowow@gmail.com>
2022-08-13 15:30:01 -0300

    feature(card-form): finished form input styling and button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { InsertCardComponent } from './insert-card/insert-card.component';
 
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSelectModule } from '@angular/material/select';
 
 const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatFormFieldModule,
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatSelectModule,
 ];
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [AppComponent, InsertCardComponent],
-  imports: [...MATERIAL_MODULES, BrowserModule],
-  providers: [],
+  imports: [...MATERIAL_MODULES, BrowserModule, BrowserAnimationsModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/insert-card/insert-card.component.ts
+++ src/app/insert-card/insert-card.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-insert-card',
   templateUrl: './insert-card.component.html',
-  styleUrls: ['./insert-card.component.scss']
+  styleUrls: ['./insert-card.component.scss'],
 })
 export class InsertCardComponent implements OnInit {
   public assetsPath = '../../assets/';
+  public cardForm!: FormGroup;
 
-  constructor() { }
+  public cardNumber = '**** **** **** ****';
+  public paymentTerm: Array<number> = [];
+  public paymentValue = 12000;
+  public selected = 1;
 
-  ngOnInit() {
+  constructor() {
+    this.paymentTerm = Array(12)
+      .fill(1)
+      .map((_, i) => i);
   }
 
+  ngOnInit(): void {
+    this.cardForm = new FormGroup({
+      cardNumber: new FormControl('', [Validators.pattern(/^[0-9]{16}$/)]),
+      cardHolderName: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z ]+$/),
+      ]),
+      cardExpirationDate: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{2}\/[0-9]{2}$/),
+      ]),
+      cardCvv: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{3}$/),
+      ]),
+    });
+  }
 }
